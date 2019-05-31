9bd724cb22c7ee9079336e2cb880a9fa1a12b0ee
hirata-dat <49702207+hirata-dat@users.noreply.github.com>
2019-05-31 16:09:16 +0900

    復習0530-01

--- fukusyu0530/Form1.Designer.cs
+++ fukusyu0530/Form1.Designer.cs
@@ -31,12 +31,14 @@
             this.components = new System.ComponentModel.Container();
             this.label1 = new System.Windows.Forms.Label();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(123, 133);
+            this.label1.Location = new System.Drawing.Point(362, 147);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(49, 12);
             this.label1.TabIndex = 0;
@@ -48,11 +50,30 @@
             this.timer1.Enabled = true;
             this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
             // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(198, 189);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 12);
+            this.label2.TabIndex = 1;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(163, 177);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(43, 12);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "label3()";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(484, 461);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Name = "Form1";
             this.ResumeLayout(false);
@@ -64,6 +85,8 @@
 
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
     }
 }
 
--- fukusyu0530/Form1.cs
+++ fukusyu0530/Form1.cs
@@ -12,8 +12,8 @@ namespace fukusyu0530
 {
     public partial class Form1 : Form
     {
-        int vx = -20;
-        int vy = -20;
+        int vx = -10;
+        int vy = -10;
         
         public Form1()
         {
@@ -22,27 +22,42 @@ namespace fukusyu0530
 
         private void timer1_Tick(object sender, EventArgs e)
         {
+
+            Text = MousePosition.X + "," + MousePosition.Y;//マウスの座標がわかる
+            Point p = PointToClient(MousePosition);
+
+            label3.Left = p.X-label3.Width/2;//MousePosition.X;//画面の左上（0,0）
+            label3.Top  = p.Y-label3.Height/2;//MousePosition.Y;
+            //p.X-label3.Width/2;中心のマウスにラベル３が来る
+            //= MousePosition.Y/2;
+            
+
             label1.Left += vx;
             label1.Top  += vy;
+            
             //label1.Leftが0より小さいか
-            if (label1.Left < 0)
+            if (label1.Left < 0 )//左
             {
-                //vxをvyに－1掛けた値にする
-                //vx = vx *-1;
-                //vx *= -1;どれでも可
-                vx = -vx;
+                vx = Math.Abs(vx);
             }
-            if (label1.Top < 0)
+            
+            if (label1.Top < 0 )//上
             {
                 vy = -vy;
             }
 
-            if (label1.Right > 500)//lade1.right647より大きいか
+            if (label1.Left > ClientSize.Width - label1.Width)//画面に合わせて跳ね返る
+            {
+                vx = -Math.Abs(vx);
+            }
+
+
+            if (label1.Right > 500)//右
             {
                 vx = -vx;
             }
 
-            if (label1.Bottom > 500)//lade1.Bottomより大きいか
+            if (label1.Bottom > 500)//下
             {
                 vy = -vy;
             }
@@ -50,15 +65,12 @@ namespace fukusyu0530
         }
         private void label1_Click(object sender, EventArgs e)
         {
-            label1.Left += vx;
-            label1.Top  += vy;
-
-
-
-
+            
             MessageBox.Show("" + ClientSize.Width + "," + ClientSize.Height);//ClientSize.Width：フォームの幅、ClientSize.Height：フォームの高さ
             MessageBox.Show("" + label1.Width + "," + label1.Handle);//label1.Width：ラベルの幅（横）、label1.Handle：ラベルの高さ（縦）
-        
+            //Width =幅(横)
+            //Height=高さ(縦)
+            //ClientSize(フォーム)
         
         
         
