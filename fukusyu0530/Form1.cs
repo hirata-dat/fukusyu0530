9c27c5b04932b48612eed040c823e7d87d773d2f
hirata-dat <49702207+hirata-dat@users.noreply.github.com>
2019-05-31 13:31:11 +0900

    復習0530

--- fukusyu0530/Form1.cs
+++ fukusyu0530/Form1.cs
@@ -12,9 +12,23 @@ namespace fukusyu0530
 {
     public partial class Form1 : Form
     {
+        int vx =-5;
+        int vy = 0;
+        
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            label1.Left += vx;
+            label1.Top  += vy;
+           
+        }
+        private void label1_Click(object sender, EventArgs e)
+        {
+            
         }
     }
 }
